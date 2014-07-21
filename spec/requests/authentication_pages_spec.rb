1cf2bc7841e488ac21667839c0a4d5ed7b521b03
TomasEpson <tomasepson@gmail.com>
2014-07-21 12:29:57 -0700

    Add user following

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -59,6 +59,18 @@ describe "AuthenticationPages" do
 					click_button "Sign in"
 				end
 
+			describe "in the Relationships controller" do
+				describe "submitting to the create action" do
+					before { post relationships_path }
+					specify { expect(response).to redirect_to(signin_path) }
+				end
+
+				describe "submitting to the destroy action" do
+					before { delete relationship_path(1) }
+					specify { expect(response).to redirect_to(signin_path) }
+				end
+			end				
+
 				describe "after signing in" do
 
 					it "should render the desired protected page" do
@@ -84,6 +96,16 @@ describe "AuthenticationPages" do
 					it { should have_title('Sign in') }
 				end
 
+				describe "visiting the following page" do
+					before { visit following_user_path(user) }
+					it { should have_title('Sign in') }
+				end
+
+				describe "visiting the followers page" do
+					before { visit followers_user_path(user) }
+					it { should have_title('Sign in') }
+				end
+
 			end
 
 			describe "in the Microposts controller" do
@@ -98,6 +120,7 @@ describe "AuthenticationPages" do
 					specify { expect(response).to redirect_to(signin_path) }
 				end
 			end
+
 		end
 
 		describe "as wrong user" do
